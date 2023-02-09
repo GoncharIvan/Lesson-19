2ffa20d97f59aa0902dd78d66c30b3e1b2eb200a
Ivan <Gonchivan1@gmail.com>
2023-02-09 13:48:16 +0200

    second commit

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-// let school = 17;
-// let university = 25;
-// let job = 60;
-// let answer = prompt ("How old are you?");
-// if (school >= +answer) {confirm("Do you study at school?");
-// } else if (school < +answer && +answer < university) {confirm ("Do you study at university?");
-// } else if (university <= +answer && +answer < job) {confirm ("Do you have a job?");
-// } else confirm ("What are you doing?");
+let school = 17;
+let university = 25;
+let job = 60;
+let answer = prompt ("How old are you?");
+if (school >= +answer) {confirm("Do you study at school?");
+} else if (school < +answer && +answer < university) {confirm ("Do you study at university?");
+} else if (university <= +answer && +answer < job) {confirm ("Do you have a job?");
+} else confirm ("What are you doing?");
 
 
-// let display = 'block'
-// let flex = 'flex'
-// let grid = 'grid'
-// let inline = 'inline'
-// let none = 'none'
+let display = 'block'
+let flex = 'flex'
+let grid = 'grid'
+let inline = 'inline'
+let none = 'none'
 
-const answer = prompt('Enter property display')
-switch(answer){
-    case 'block':
-        alert('display:block');
-        break;
-    case 'flex':
-        alert('display:flex');
-        break;
-    case 'grid':
-        alert("display:grid" );
-        break;
-    case 'inline':
-        alert('display:inline' );
-        break;
-    case 'none':
-        alert('display:none');
-        break;
-    default:
-        alert("Set 'display:inline block;'");
- }
+
+
+
+// const answer = prompt('Enter property display')
+// switch(answer){
+//     case 'block':
+//         alert('display:block');
+//         break;
+//     case 'flex':
+//         alert('display:flex');
+//         break;
+//     case 'grid':
+//         alert("display:grid" );
+//         break;
+//     case 'inline':
+//         alert('display:inline' );
+//         break;
+//     case 'none':
+//         alert('display:none');
+//         break;
+//     default:
+//         alert("Set 'display:inline block;'");
+//  }
 
 
 
@@ -49,6 +52,8 @@ switch(answer){
 // alert(`${+answer} by 12 equals ${+answer * num}`);
 
  
+
+
 
 // for (let i = 0; i < 52; i++) {
 //   if(i % 2 === 0) {
